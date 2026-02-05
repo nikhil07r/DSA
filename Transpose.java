e2a3298811cbb2f9120ae6886ad74494dc28a11d
nikhil07r <nikhilrawat20062021@gmail.com>
2026-02-05 13:14:40 +0530

    Update

--- Transpose.java
+++ Transpose.java
@@ -7,7 +7,6 @@ public class Transpose {
             System.out.println();
         }
     }
-
     public static void main(String[] args) {
 
         int[][] x = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
